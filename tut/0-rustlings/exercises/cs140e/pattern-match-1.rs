18fe4c037cf11c1380f71deecf942db4b2546470
tristanburgess <tburgessdev@gmail.com>
2020-12-28 09:54:09 -0500

    lab0: started rustlings exercises

--- tut/0-rustlings/exercises/cs140e/pattern-match-1.rs
+++ tut/0-rustlings/exercises/cs140e/pattern-match-1.rs
@@ -1,18 +1,39 @@
 // FIXME: Make me compile! Diff budget: 2 lines.
 
-// I AM NOT DONE
-
 // Do not change this definition.
+macro_rules! is_enum_variant {
+    ($v:expr, $p:pat) => {
+        if let $p = $v { true } else { false }
+    };
+}
+
 enum MyEnum {
     A(String),
     B(String),
 }
 
 fn matcher(val: &MyEnum) -> &str {
-    match *val {
+    match &val {
         MyEnum::A(string) => string.as_str(),
         MyEnum::B(string) => string.as_str()
     }
 }
 
-fn main() {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a_matcher() {
+        let test = MyEnum::A(String::from("abc"));
+        assert!(is_enum_variant!(test, MyEnum::A { .. }));
+        assert_eq!(String::from("abc"), matcher(&test));
+    }
+
+    #[test]
+    fn b_matcher() {
+        let test = MyEnum::B(String::from("abc"));
+        assert!(is_enum_variant!(test, MyEnum::B { .. }));
+        assert_eq!(String::from("abc"), matcher(&test));
+    }
+}
