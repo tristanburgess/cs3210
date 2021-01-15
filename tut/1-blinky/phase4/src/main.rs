1913eeb0b89086c46d090fd270cf193c0a52ced3
tristanburgess <tburgessdev@gmail.com>
2021-01-15 00:28:48 -0500

    lab1: all done, got blinky lights in C and Rust

--- tut/1-blinky/phase4/src/main.rs
+++ tut/1-blinky/phase4/src/main.rs
@@ -13,6 +13,8 @@ const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
 const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
 const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
 
+const BLINK_SLEEP_MS: usize = 150;
+
 #[inline(never)]
 fn spin_sleep_ms(ms: usize) {
     for _ in 0..(ms * 6000) {
@@ -23,5 +25,11 @@ fn spin_sleep_ms(ms: usize) {
 unsafe fn kmain() -> ! {
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
-    loop {}
+    GPIO_FSEL1.write_volatile(0x1 << 18);
+    loop {
+        GPIO_SET0.write_volatile(0x1 << 16);
+        spin_sleep_ms(BLINK_SLEEP_MS);
+        GPIO_CLR0.write_volatile(0x1 << 16);
+        spin_sleep_ms(BLINK_SLEEP_MS);
+    }
 }
