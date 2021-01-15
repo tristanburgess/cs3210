ab99fb9cfd924aae238fd6769d836d35b94b1b07
tristanburgess <tburgessdev@gmail.com>
2021-01-15 00:28:48 -0500

    lab1: all done, got blinky lights in C and Rust

--- tut/1-blinky/phase3/blinky.c
+++ tut/1-blinky/phase3/blinky.c
@@ -4,6 +4,8 @@ volatile unsigned *GPIO_FSEL1 = (volatile unsigned *)(GPIO_BASE + 0x04);
 volatile unsigned *GPIO_SET0  = (volatile unsigned *)(GPIO_BASE + 0x1C);
 volatile unsigned *GPIO_CLR0  = (volatile unsigned *)(GPIO_BASE + 0x28);
 
+#define BLINK_SLEEP_MS 150
+
 static void spin_sleep_us(unsigned int us) {
   for (unsigned int i = 0; i < us * 6; i++) {
     asm volatile("nop");
@@ -15,6 +17,32 @@ static void spin_sleep_ms(unsigned int ms) {
 }
 
 int kmain(void) {
-  // FIXME: STEP 1: Set GPIO Pin 16 as output.
-  // FIXME: STEP 2: Continuously set and clear GPIO 16.
+  /*
+  Q: Now, read the documentation for GPFSELn register on pages 91 and 92. 
+  We write to this register to set up a pin as an output or input. 
+  Which value to which field in register GPFSEL1 must be written 
+    so that GPIO pin 16 is set as an output?
+
+  A: Write 0x1 to field FSEL16
+  */
+
+  /*
+  Q: Now, read the documentation for the GPSET0 and GPCLR0 registers on page 95. 
+  We write to GPSET0 to set a pin (turn it on) and write to GPCLR0 to 
+    clear a pin (turn it off). 
+  Which value do we write to which field in these registers to set/clear pin 16?
+
+  A: Write 0x1 to field SET16 in register GPSET0 and 
+    0x1 to field CLEAR16 in registerGPCLR0.
+  */
+
+  // STEP 1: Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 = 0x1 << 18;
+  // STEP 2: Continuously set and clear GPIO 16.
+  while (1) {
+    *GPIO_SET0 = 0x1 << 16;
+    spin_sleep_ms(BLINK_SLEEP_MS);
+    *GPIO_CLR0 = 0x1 << 16;
+    spin_sleep_ms(BLINK_SLEEP_MS);
+  }
 }
--- tut/1-blinky/phase4/src/main.rs
+++ tut/1-blinky/phase4/src/main.rs
@@ -13,6 +13,8 @@ const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
 const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
 const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
 
+const BLINK_SLEEP_MS: usize = 150;
+
 #[inline(never)]
 fn spin_sleep_ms(ms: usize) {
     for _ in 0..(ms * 6000) {
@@ -21,7 +23,13 @@ fn spin_sleep_ms(ms: usize) {
 }
 
 unsafe fn kmain() -> ! {
-    // FIXME: STEP 1: Set GPIO Pin 16 as output.
-    // FIXME: STEP 2: Continuously set and clear GPIO 16.
-    loop {}
+    // STEP 1: Set GPIO Pin 16 as output.
+    GPIO_FSEL1.write_volatile(0x1 << 18);
+    // STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(0x1 << 16);
+        spin_sleep_ms(BLINK_SLEEP_MS);
+        GPIO_CLR0.write_volatile(0x1 << 16);
+        spin_sleep_ms(BLINK_SLEEP_MS);
+    }
 }
